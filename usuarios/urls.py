b9a447efe1447d3f6477b8c5bb106eb2c5f65959
agustin carranza <agustincarranzacarabajal@gmail.com>
2024-08-13 20:07:04 -0300

    segundo commit

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -5,7 +5,7 @@ from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path ("login/", views.login_request, name="Login"),
-    path("register", views.register, name= "Register"),
+    path("register/", views.register, name= "Register"),
     path("logout/", LogoutView.as_view(template_name="appfinal/inicio.html"), name= "Logout"),
 
 ]
